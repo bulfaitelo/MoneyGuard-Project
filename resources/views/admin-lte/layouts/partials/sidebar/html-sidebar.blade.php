b050a4feb2828271bb58741f7f9107764a9092b1
Bulfaitelo <Bulfaitelo@gmail.com>
2019-10-13 10:42:07 -0300

    img correction

--- resources/views/admin-lte/layouts/partials/sidebar/html-sidebar.blade.php
+++ resources/views/admin-lte/layouts/partials/sidebar/html-sidebar.blade.php
@@ -2,7 +2,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="{{ route('home')}}" class="brand-link">
-          <img src="imgs/moneyguard_logo.png"
+          <img src="/imgs/moneyguard_logo.png"
                alt="AdminLTE Logo"
                class="brand-image img-circle elevation-3"
                style="opacity: .8">
