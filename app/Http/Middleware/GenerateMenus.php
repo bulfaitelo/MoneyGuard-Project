ca747adf9a2ec3e35c8e6b154e1ba02d656e7c52
Bulfaitelo <Bulfaitelo@gmail.com>
2019-10-13 09:34:25 -0300

    done basic menu configuration

--- app/Http/Middleware/GenerateMenus.php
+++ app/Http/Middleware/GenerateMenus.php
@@ -16,45 +16,86 @@ class GenerateMenus
     public function handle($request, Closure $next)
     {
         \Menu::make('externalLinks', function ($menu) {
-            $menu->add('Demo Gentelella')->link->href('https://colorlib.com/polygon/gentelella/index.html')->attr(['target' => '_blank']);
-            $menu->add('Gentelella Git')->link->href('https://github.com/puikinsh/gentelella')->attr(['target' => '_blank']);
-            $menu->add('Gentelella Documentation')->link->href('https://puikinsh.github.io/gentelella/')->attr(['target' => '_blank']);
-            $menu->add('Laravel Menu')->link->href('https://github.com/lavary/laravel-menu#links-href-property')->attr(['target' => '_blank']);
-            $menu->add('jQuery Sparklines')->link->href('https://omnipotent.net/jquery.sparkline/#s-about')->attr(['target' => '_blank']);
-            $menu->add('Chartjs')->link->href('http://www.chartjs.org/')->attr(['target' => '_blank']);            
-            $menu->add('Date Range Picker')->link->href('http://www.daterangepicker.com/#ex5')->attr(['target' => '_blank']);            
-            $menu->add('Data Tables')->link->href('https://datatables.net')->attr(['target' => '_blank']);            
+
+            $menu->group(['class'=> 'nav-item'], function ($m){
+                $m->add('Demo Gentelella')->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->href('https://colorlib.com/polygon/gentelella/index.html')
+                    ->attr(['target' => '_blank', 'class' => 'nav-link']);
+                $m->add('Gentelella Git')->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->href('https://github.com/puikinsh/gentelella')
+                    ->attr(['target' => '_blank', 'class' => 'nav-link']);
+                $m->add('Gentelella Documentation')->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->href('https://puikinsh.github.io/gentelella/')
+                    ->attr(['target' => '_blank', 'class' => 'nav-link']);
+                $m->add('Laravel Menu')->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->href('https://github.com/lavary/laravel-menu#links-href-property')
+                    ->attr(['target' => '_blank', 'class' => 'nav-link']);
+                $m->add('jQuery Sparklines')->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->href('https://omnipotent.net/jquery.sparkline/#s-about')
+                    ->attr(['target' => '_blank', 'class' => 'nav-link']);
+                $m->add('Chartjs')->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->href('http://www.chartjs.org/')
+                    ->attr(['target' => '_blank', 'class' => 'nav-link']);            
+                $m->add('Date Range Picker')->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->href('http://www.daterangepicker.com/#ex5')
+                    ->attr(['target' => '_blank', 'class' => 'nav-link']);            
+                $m->add('Data Tables')->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->href('https://datatables.net')
+                    ->attr(['target' => '_blank', 'class' => 'nav-link']);            
+            });
             
         });
 
         \Menu::make('mainMenu', function ($menu) {
-            $menu->add('Home', 'home');
-            $menu->add('test', 'test');
+            $menu->group(['class'=> 'nav-item'], function ($m){
+                $m->add('Home', 'home')->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->attr(['class'=> 'nav-link']);
+                $m->add('test', 'test')->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->attr(['class'=> 'nav-link']);               
+            });
         });
 
         \Menu::make('ativosMenu', function ($menu) {
-            $menu->add('Home', ['route' =>'index']);
-            $menu->add('Analítico', ['route' =>'analitico.index']);
-            $menu->add('Protocolos', [ 'route' => 'protocolos.index']);
-            $menu->add('Preços', [ 'route' => 'precos.index']);
+            $menu->group(['class'=> 'nav-item'], function ($m){
+                $m->add('Home', ['route' =>'index'])->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->attr(['class'=> 'nav-link']);
+                $m->add('Analítico', ['route' =>'analitico.index'])->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->attr(['class'=> 'nav-link']);
+                $m->add('Protocolos', [ 'route' => 'protocolos.index'])->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->attr(['class'=> 'nav-link']);
+                $m->add('Preços', [ 'route' => 'precos.index'])->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->attr(['class'=> 'nav-link']);
+            });
         });
 
         \Menu::make('santanderMenu', function ($menu) {
-            $menu->add('Home', ['route'=> 'santander.index']);
-            $menu->add('Aniversário', ['route'=> 'santander.aniversario']);
-            $menu->add('Movimentação', ['route'=> 'santander.movimentacao']);
+            $menu->group(['class'=> 'nav-item'], function ($m){
+                $m->add('Home', ['route'=> 'santander.index'])->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->attr(['class'=> 'nav-link']);
+                $m->add('Aniversário', ['route'=> 'santander.aniversario'])->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->attr(['class'=> 'nav-link']);
+                $m->add('Movimentação', ['route'=> 'santander.movimentacao'])->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->attr(['class'=> 'nav-link']);
+            });
         });
 
         
 
         \Menu::make('logsMenu', function ($menu) {
-            $menu->add('Importação', ['route'=>'logs.import']);
-            $menu->add('Backup', ['route'=>'logs.backup']);
+            $menu->group(['class'=> 'nav-item'], function ($m){
+                $m->add('Importação', ['route'=>'logs.import'])->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->attr(['class'=> 'nav-link']);
+                $m->add('Backup', ['route'=>'logs.backup'])->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->attr(['class'=> 'nav-link']);
+            });
         });
         \Menu::make('parametrosMenu', function ($menu) {
-            $menu->add('Títulos', [ 'route' => 'titulo.index']);
-            $menu->add('Schedule', [ 'route' => 'schedule.index']);
-
+            $menu->group(['class'=> 'nav-item'], function ($m){
+                $m->add('Títulos', [ 'route' => 'titulo.index'])->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->attr(['class'=> 'nav-link']);
+                $m->add('Schedule', [ 'route' => 'schedule.index'])->prepend('<i class="far fa-circle nav-icon" > </i>')
+                    ->link->attr(['class'=> 'nav-link']);
+            });
         });
         
     
