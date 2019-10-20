e208fa944e847179f3e081e4c112c165f381f0f5
Bulfaitelo <Bulfaitelo@gmail.com>
2019-10-20 10:22:25 -0300

    add App id on wrapper

--- resources/views/admin-lte/layouts/partials/wrapper/html-wrapper.blade.php
+++ resources/views/admin-lte/layouts/partials/wrapper/html-wrapper.blade.php
@@ -16,7 +16,7 @@
         </section>
     
         <!-- Main content -->
-        <section class="content">
+        <section class="content" id="app">
             @yield('content')
          
     
