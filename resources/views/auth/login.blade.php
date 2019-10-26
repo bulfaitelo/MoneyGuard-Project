210d7253f8c76fd708631e7fb84e49d31a895282
Bulfaitelo <Bulfaitelo@gmail.com>
2019-10-26 17:44:15 -0300

    login blade correction

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,32 +17,30 @@
       <form method="POST" action="{{ route('login') }}">
           @csrf
         <div class="input-group mb-3">
-          <input id="email" type="email" placeholder="E-mail" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-          @if ($errors->has('email'))
-              <span class="invalid-feedback">
-                  <strong>{{ $errors->first('email') }}</strong>
-              </span>
-          @endif
+          <input id="email" type="email" placeholder="E-mail" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>          
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
             </div>
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <input id="password" type="password" placeholder="Password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-          @if ($errors->has('password'))
+          @if ($errors->has('email'))
               <span class="invalid-feedback">
-                  <strong>{{ $errors->first('password') }}</strong>
+                  <strong>{{ $errors->first('email') }}</strong>
               </span>
           @endif
+        </div>
+        <div class="input-group mb-3">
+          <input id="password" type="password" placeholder="Password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>          
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
             </div>
           </div>
+          @if ($errors->has('password'))
+              <span class="invalid-feedback">
+                  <strong>{{ $errors->first('password') }}</strong>
+              </span>
+          @endif
         </div>
         <div class="row">
           <div class="col-8">
